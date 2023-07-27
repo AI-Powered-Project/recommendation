20abfe8bb47fdbbcd425dbf9be2290c30513be1f
juni5184 <juni5184@naver.com>
2023-07-27 12:09:37 -0700

    start cosign similarity

---
+++ similarity_cosign.py
@@ -0,0 +1,46 @@
+import json
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+file_path = './user_data.json'
+output_file_path = './user_data_similarity_2.json'
+
+with open(file_path, 'r') as file:
+    users = json.load(file)
+
+
+# 벡터화를 위한 전처리 함수
+def preprocess_preferences(preferences):
+    return ' '.join(preferences)
+
+# 벡터화를 위한 전처리된 데이터 생성
+preferences_list = [preprocess_preferences(user['general_preference']) + ' ' +
+                    preprocess_preferences(user['category_preference'])
+                    for user in users]
+
+# 벡터화 수행
+vectorizer = CountVectorizer()
+preferences_matrix = vectorizer.fit_transform(preferences_list)
+
+# 코사인 유사도 계산
+cosine_similarities = cosine_similarity(preferences_matrix)
+
+# 유사도 결과 출력
+for i, user1 in enumerate(users):
+    for j, user2 in enumerate(users):
+        similarity_score = cosine_similarities[i, j]
+        print(f"Cosine Similarity : {user1['user_id']} and {user2['user_id']}: {similarity_score:.3f}")
+        print(similarity_score)
+
+
+
+# # Update the existing JSON data with similarity information
+# for user in users:
+#     user_id = user['user_id']
+#     user['similarity'] = user_similarities[user_id]
+#
+# # Save the updated JSON data back to the file
+# with open(output_file_path, 'w') as file:
+#     json.dump(users, file, indent=2)
