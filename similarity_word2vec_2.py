ac7fc2a9c1e0b19065200ea798a665b57eba2fb4
juni5184 <juni5184@naver.com>
2023-08-09 12:00:14 -0700

    word2vec second method but has to be fix

--- similarity_word2vec_2.py
+++ similarity_word2vec_2.py
@@ -9,8 +9,16 @@ file_path = './data/user_data_modify.json'
 output_file_path = './data/similarity_w2v.json'
 
 with open(file_path, 'r') as file:
-    user_data = json.load(file)
+    users = json.load(file)
 
+
+# 사용자의 preference를 추출하여 리스트로 저장
+all_preferences = []
+for user in users:
+    preferences = user["general_preferences"] + user["category_preference"]
+    all_preferences.append(preferences)
+
+print(all_preferences)
 # Load the trained Word2Vec model
 model = Word2Vec.load("word2vec_model.bin")
 
@@ -22,14 +30,14 @@ model = Word2Vec.load("word2vec_model.bin")
 #             sentences.append([key, value['selected'], value['opposite']])
 #     sentences.extend(user['category_preference'])
 
-# # Train Word2Vec model
-# model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, sg=0)
+# Train Word2Vec model
+model = Word2Vec(sentences=all_preferences, vector_size=100, window=5, min_count=1, sg=0)
 
-# # Save the trained model
-# # model.save("word2vec_model.bin")
+# Save the trained model
+model.save("word2vec_model.bin")
 
-# # Load the trained model
-# model = Word2Vec.load("word2vec_model.bin")
+# Load the trained model
+model = Word2Vec.load("word2vec_model.bin")
 
 # Calculate average vector for user preferences with weighted selected and opposite preferences
 def calculate_weighted_average_vector(preferences, selected_weight, opposite_weight):
@@ -48,12 +56,27 @@ def calculate_weighted_average_vector(preferences, selected_weight, opposite_wei
     return total_vector / count
 
 # Calculate and print similarity between users
-for i, user1 in enumerate(user_data):
-    for j, user2 in enumerate(user_data[i+1:], i+1):
+user_similarities = {user['user_id']: {} for user in users}  # Initialize user_similarities for all user IDs
+for i, user1 in enumerate(users):
+    for j, user2 in enumerate(users[i:], i+1):
         user1_vector = calculate_weighted_average_vector(user1['general_preferences'], selected_weight=2, opposite_weight=0.5)
         user2_vector = calculate_weighted_average_vector(user2['general_preferences'], selected_weight=2, opposite_weight=0.5)
         if user1_vector is not None and user2_vector is not None:
             user1_vector = user1_vector.reshape(1, -1)
             user2_vector = user2_vector.reshape(1, -1)
             similarity = cosine_similarity(user1_vector, user2_vector)[0][0]
-            print(f"Similarity between User {user1['user_id']} and User {user2['user_id']}: {similarity}")
+            user_similarities[user1['user_id']][user2['user_id']] = round(similarity *10, 3)
+            user_similarities[user2['user_id']][user1['user_id']] = round(similarity *10, 3)
+            # print(f"Similarity between User {user1['user_id']} and User {user2['user_id']}: {similarity}")
+
+
+
+# Update the existing JSON data with similarity information
+for user in users:
+    user_id = user['user_id']
+    user['similarity'] = user_similarities[user_id]
+
+# Save the updated JSON data back to the file
+with open(output_file_path, 'w') as file:
+    json.dump(users, file, indent=2)
+
