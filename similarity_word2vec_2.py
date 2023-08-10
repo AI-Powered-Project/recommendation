1026cc8830b3cdf0f0855de3b2c9ca8ee56c443a
juni5184 <juni5184@naver.com>
2023-08-10 15:44:46 -0700

    general preference input fixed

--- similarity_word2vec_2.py
+++ similarity_word2vec_2.py
@@ -5,25 +5,25 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-file_path = './data/user_data_modify.json'
+file_path = './data/user_data_modify_before.json'
 output_file_path = './data/similarity_w2v.json'
 
 with open(file_path, 'r') as file:
-    users = json.load(file)
+    user_data = json.load(file)
 
 # Load the trained Word2Vec model
 model = Word2Vec.load("word2vec_model.bin")
 
 # Prepare text data for Word2Vec training
-# sentences = []
-# for user in user_data:
-#     for preference in user['general_preferences']:
-#         for key, value in preference.items():
-#             sentences.append([key, value['selected'], value['opposite']])
-#     sentences.extend(user['category_preference'])
+sentences = []
+for user in user_data:
+    for preference in user['general_preference']:
+        for key, value in preference.items():
+            sentences.append([key, value['selected'], value['opposite']])
+    sentences.extend(user['category_preference'])
 
 # # Train Word2Vec model
-# model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, sg=0)
+model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, sg=0)
 
 # # Save the trained model
 # # model.save("word2vec_model.bin")
@@ -36,6 +36,7 @@ def calculate_weighted_average_vector(preferences, selected_weight, opposite_wei
     total_vector = np.zeros(model.vector_size)
     count = 0
     for preference in preferences:
+        print(preference)
         for key, value in preference.items():
             try:
                 total_vector += selected_weight * model.wv[value['selected']]
@@ -48,27 +49,21 @@ def calculate_weighted_average_vector(preferences, selected_weight, opposite_wei
     return total_vector / count
 
 # Calculate and print similarity between users
-user_similarities = {user['user_id']: {} for user in users}  # Initialize user_similarities for all user IDs
-for i, user1 in enumerate(users):
-    for j, user2 in enumerate(users[i:], i+1):
-        user1_vector = calculate_weighted_average_vector(user1['general_preferences'], selected_weight=2, opposite_weight=0.5)
-        user2_vector = calculate_weighted_average_vector(user2['general_preferences'], selected_weight=2, opposite_weight=0.5)
-        if user1_vector is not None and user2_vector is not None:
-            user1_vector = user1_vector.reshape(1, -1)
-            user2_vector = user2_vector.reshape(1, -1)
-            similarity = cosine_similarity(user1_vector, user2_vector)[0][0]
-            user_similarities[user1['user_id']][user2['user_id']] = round(similarity *10, 3)
-            user_similarities[user2['user_id']][user1['user_id']] = round(similarity *10, 3)
-            # print(f"Similarity between User {user1['user_id']} and User {user2['user_id']}: {similarity}")
+# for i, user1 in enumerate(user_data):
+    # for j, user2 in enumerate(user_data[i+1:], i+1):
+user1 = user_data[0]
+user2 = user_data[1]
+
+user1_vector = calculate_weighted_average_vector(user1['general_preference'], selected_weight=2, opposite_weight=0.5)
+user2_vector = calculate_weighted_average_vector(user2['general_preference'], selected_weight=2, opposite_weight=0.5)
 
 
+user1_category_pref = user1["category_preference"]
+user2_category_pref = user2["category_preference"]
 
-# Update the existing JSON data with similarity information
-for user in users:
-    user_id = user['user_id']
-    user['similarity'] = user_similarities[user_id]
 
-# Save the updated JSON data back to the file
-with open(output_file_path, 'w') as file:
-    json.dump(users, file, indent=2)
-
+if user1_vector is not None and user2_vector is not None:
+    user1_vector = user1_vector.reshape(1, -1)
+    user2_vector = user2_vector.reshape(1, -1)
+    similarity = cosine_similarity(user1_vector, user2_vector)[0][0]
+    print(f"Similarity between User {user1['user_id']} and User {user2['user_id']}: {similarity}")
