9f990494c911990f200e761dec4a19a4cd9b028b
juni5184 <juni5184@naver.com>
2023-08-10 21:36:27 -0700

    general fin -> backend update

--- model/general_model.py
+++ model/general_model.py
@@ -10,10 +10,9 @@ with open(file_path, 'r') as file:
 
 # Prepare text data for Word2Vec training
 sentences = []
-for user in user_data:
-    for preference in user['general_preference']:
-        for key, value in preference.items():
-            sentences.append([value['selected'], value['opposite']])
+for preference in user_data[0]['general_preference']:
+    for key, value in preference.items():
+        sentences.append([value['selected'], value['opposite']])
 # print(sentences)
 
 # # # Train Word2Vec model
@@ -21,15 +20,18 @@ model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, sg=0)
 
 # Save the trained model
 model.save("general_model.bin")
+# print(model.wv.key_to_index) # -> key값 + idx
+
 
 
 #------------------------ visualization ---------------------
 
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Word2Vec model load
+# # Word2Vec model load
 model = Word2Vec.load("general_model.bin")  # Model path.
 
 # Extracting words and vectors from the model
@@ -41,13 +43,27 @@ perplexity_value = min(30, len(words) - 1)
 tsne = TSNE(n_components=2, random_state=42, perplexity=perplexity_value)
 reduced_vectors = tsne.fit_transform(vectors)
 
-# visualization
+# 단어 간 의미적 유사도 계산
+similarities = cosine_similarity(vectors)
+
+# 군집 형성
+threshold = 0.8  # 유사도 임계값
+clusters = {word: [i] for i, word in enumerate(words)}
+for i in range(len(words)):
+    for j in range(i + 1, len(words)):
+        if similarities[i, j] > threshold:
+            clusters[words[i]].append(j)
+            clusters[words[j]].append(i)
+
+# # visualization
 plt.figure(figsize=(12, 8))
-plt.scatter(reduced_vectors[:, 0], reduced_vectors[:, 1], marker='o', color='b', alpha=0.7)
+for cluster_indices in clusters.values():
+    plt.scatter(reduced_vectors[cluster_indices, 0], reduced_vectors[cluster_indices, 1], marker='o', alpha=0.7)
+
 for i, word in enumerate(words):
     plt.annotate(word, (reduced_vectors[i, 0], reduced_vectors[i, 1]), fontsize=9)
 
-plt.title('Word2Vec Word Embedding Visualization')
+plt.title('Word2Vec Word Embedding Clustering Visualization')
 plt.xlabel('Dimension 1')
 plt.ylabel('Dimension 2')
 plt.show()
