c7aa2a100a92fc695254b389c2303869ab605c0f
juni5184 <juni5184@naver.com>
2023-08-10 20:18:22 -0700

    add model visualization

--- model/general_model.py
+++ model/general_model.py
@@ -1,6 +1,7 @@
 from gensim.models import Word2Vec
 import json
 
+#------------------------ make general_preference model ---------------------
 
 file_path = '../data/user_general.json'
 with open(file_path, 'r') as file:
@@ -22,3 +23,31 @@ model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, sg=0)
 model.save("general_model.bin")
 
 
+#------------------------ visualization ---------------------
+
+import numpy as np
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+# Word2Vec model load
+model = Word2Vec.load("general_model.bin")  # Model path.
+
+# Extracting words and vectors from the model
+words = list(model.wv.index_to_key)
+vectors = np.array([model.wv[word] for word in words])  # Convert to 2D array
+
+# Use t-SNE to reduce vectors in two dimensions
+perplexity_value = min(30, len(words) - 1)
+tsne = TSNE(n_components=2, random_state=42, perplexity=perplexity_value)
+reduced_vectors = tsne.fit_transform(vectors)
+
+# visualization
+plt.figure(figsize=(12, 8))
+plt.scatter(reduced_vectors[:, 0], reduced_vectors[:, 1], marker='o', color='b', alpha=0.7)
+for i, word in enumerate(words):
+    plt.annotate(word, (reduced_vectors[i, 0], reduced_vectors[i, 1]), fontsize=9)
+
+plt.title('Word2Vec Word Embedding Visualization')
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.show()
